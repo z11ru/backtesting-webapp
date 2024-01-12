ddf0403c73ebd9792b11e37f95447c326651603c
z11ru <z11ru@outlook.com>
2024-01-12 15:25:44 -0700

    simple bollinger strategy

--- verify_data.py
+++ verify_data.py
@@ -1,11 +1,9 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-# Define the stock ticker symbol
-ticker_symbol = 'TSLA'  # Example: Apple Inc.
+ticker_symbol = 'TSLA'
 stock_data = pd.read_csv('TSLA_stock_data.csv')
 
-# Plotting the closing price of the stock
 stock_data['Close'].plot(figsize=(10, 6))
 plt.title(f'Closing Price of {ticker_symbol}')
 plt.xlabel('Date')
