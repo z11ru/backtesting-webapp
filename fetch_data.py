de5d4c9e86ec875ae56f1e7442775e193ce14283
z11ru <z11ru@outlook.com>
2024-01-16 09:19:05 -0700

    update

--- fetch_data.py
+++ fetch_data.py
@@ -2,19 +2,18 @@ import pandas as pd
 import yfinance as yf
 import matplotlib.pyplot as plt
 
-ticker_symbol = 'TSLA'
+symbols = {'TSLA', 'MSFT', 'AAPL', 'NVDA', 'META', 'GOOG', 'AMZN'}
 
-stock_data = yf.download(ticker_symbol, start='2008-01-01', end='2024-01-01')
-file_name = f'{ticker_symbol}_stock_data.csv'
-stock_data.to_csv(file_name, index=True)
+for symbol in symbols:
+    stock_data = yf.download(symbol, start='2020-01-01', end='2024-01-01')
+    file_name = f'{symbol}_stock_data.csv'
+    stock_data.to_csv(file_name, index=True)
 
 print(stock_data.head())
 
-ticker_symbol = 'TSLA'
-stock_data = pd.read_csv('TSLA_stock_data.csv')
+stock_data = pd.read_csv('AAPL_stock_data.csv')
 
 stock_data['Close'].plot(figsize=(10, 6))
-plt.title(f'Closing Price of {ticker_symbol}')
 plt.xlabel('Date')
 plt.ylabel('Price (USD)')
 plt.show()
